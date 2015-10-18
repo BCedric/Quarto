f5b59c6c0f0fe48c799b0f124689a00c4c6f51b5
BCedric <skalimero@gmx.fr>
2015-10-18 18:44:33 +0200

    Mise en place de l'entité Joueur

--- src/Jeu/Jeu.java
+++ src/Jeu/Jeu.java
@@ -10,9 +10,13 @@ import CaracteristiquesPiece.Taille;
 public class Jeu {
 	private Plateau plateau;
 	private ArrayList<Piece> pieces;
+	private Joueur j1;
+	private Joueur j2;
 	
 	public Jeu(){
 		Plateau plateau = new Plateau();
+		j1 = new Joueur();
+		j2 = new Joueur();
 		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.carree));
 		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.rond));
 		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.vide, Forme.carree));
@@ -29,7 +33,6 @@ public class Jeu {
 		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.plein, Forme.rond));
 		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.carree));
 		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.rond));
-		
 	}
 
 }
---
+++ src/Jeu/Joueur.java
@@ -0,0 +1,10 @@
+package Jeu;
+
+import EtatJoueur.EtatJoueur;
+
+public class Joueur {
+	private Jeu j;
+	private EtatJoueur etatCourant;
+	
+
+}
