93a5504ee7a33d8751670121ed0c616eae0a31d2
BCedric <skalimero@gmx.fr>
2015-10-22 09:18:52 +0200

    Ajout des id sur les piece

--- src/Jeu/Jeu.java
+++ src/Jeu/Jeu.java
@@ -12,29 +12,32 @@ public class Jeu {
 	private ArrayList<Piece> pieces;
 	private Joueur j1;
 	private Joueur j2;
-	private Joueur tour;
+	private Joueur actif;
 	
 	public Jeu(){
-		Plateau plateau = new Plateau();
+		this.plateau = new Plateau();
 		j1 = new Joueur(this);
 		j2 = new Joueur(this);
-		this.tour = j1;
-		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.carree));
-		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.rond));
-		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.vide, Forme.carree));
-		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.vide, Forme.rond));
-		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.plein, Forme.carree));
-		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.plein, Forme.rond));
-		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.vide, Forme.carree));
-		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.vide, Forme.rond));
-		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.plein, Forme.carree));
-		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.plein, Forme.rond));
-		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.vide, Forme.carree));
-		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.vide, Forme.rond));
-		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.plein, Forme.carree));
-		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.plein, Forme.rond));
-		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.carree));
-		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.rond));
+		j1.setEtatCourant(j1.getAttente());
+		j2.setEtatCourant(j2.getChoixPiece());
+		this.actif = j1;
+		this.pieces = new ArrayList<Piece>();
+		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.carree, 1));
+		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.plein, Forme.rond, 2));
+		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.vide, Forme.carree, 3));
+		pieces.add(new Piece(Couleur.clair, Taille.grand, Interieur.vide, Forme.rond, 4));
+		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.plein, Forme.carree, 4));
+		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.plein, Forme.rond, 6));
+		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.vide, Forme.carree, 7));
+		pieces.add(new Piece(Couleur.clair, Taille.petit, Interieur.vide, Forme.rond, 8));
+		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.plein, Forme.carree, 9));
+		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.plein, Forme.rond, 10));
+		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.vide, Forme.carree, 11));
+		pieces.add(new Piece(Couleur.sombre, Taille.grand, Interieur.vide, Forme.rond, 12));
+		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.plein, Forme.carree, 13));
+		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.plein, Forme.rond, 14));
+		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.carree, 15));
+		pieces.add(new Piece(Couleur.sombre, Taille.petit, Interieur.vide, Forme.rond, 16));
 	}
 	
 	public boolean placerPiece(Piece p, Case c){
@@ -51,6 +54,41 @@ public class Jeu {
 		}
 		return false;
 	}
+	
+	public Piece choixPiece(int n){
+		for(Piece p:this.pieces){
+			if(p.getId() == n){
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	public void changerActif(){
+		if(j1 != actif){
+			actif = j1;
+		} else {
+			actif = j2;
+		}
+	}
+	
+	public Joueur getActif() {
+		return actif;
+	}
+	
+	public ArrayList<Piece> getPieces() {
+		return pieces;
+	}
+
+	public Joueur getNonActif(){
+		if(actif == j1){
+			return j2;
+		} else return j1;
+	}
+
+	public Case choixCase(int n){
+		return this.plateau.choixCase(n);
+	}
 
 	public Joueur getJ1() {
 		return j1;
@@ -59,9 +97,5 @@ public class Jeu {
 	public Joueur getJ2() {
 		return j2;
 	}
-	
-	
-	
-	
 
 }
