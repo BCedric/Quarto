1b846b5e13d8d1c6a7b2339fc7ec3e7d079327a2
BCedric <skalimero@gmx.fr>
2015-10-22 09:15:44 +0200

    Modification de la création des cases sur le plateau

--- src/Jeu/Plateau.java
+++ src/Jeu/Plateau.java
@@ -4,25 +4,27 @@ import java.util.ArrayList;
 
 public class Plateau {
 	private ArrayList<Ligne> lignes;
+	private ArrayList<Case> cases;
 	
 	public Plateau(){
+		this.cases = new ArrayList<Case>();
 		lignes = new ArrayList<Ligne>();
-		Case c1 = new Case(1);
-		Case c2 = new Case(2);
-		Case c3 = new Case(3);
-		Case c4 = new Case(4);
-		Case c5 = new Case(5);
-		Case c6 = new Case(6);
-		Case c7 = new Case(7);
-		Case c8 = new Case(8);
-		Case c9 = new Case(9);
-		Case c10 = new Case(10);
-		Case c11 = new Case(11);
-		Case c12 = new Case(12);
-		Case c13 = new Case(13);
-		Case c14 = new Case(14);
-		Case c15 = new Case(15);
-		Case c16 = new Case(16);
+		Case c1 = this.nouvelleCase(1);
+		Case c2 = this.nouvelleCase(2);
+		Case c3 = this.nouvelleCase(3);
+		Case c4 = this.nouvelleCase(4);
+		Case c5 = this.nouvelleCase(5);
+		Case c6 = this.nouvelleCase(6);
+		Case c7 = this.nouvelleCase(7);
+		Case c8 = this.nouvelleCase(8);
+		Case c9 = this.nouvelleCase(9);
+		Case c10 = this.nouvelleCase(10);
+		Case c11 = this.nouvelleCase(11);
+		Case c12 = this.nouvelleCase(12);
+		Case c13 = this.nouvelleCase(13);
+		Case c14 = this.nouvelleCase(14);
+		Case c15 = this.nouvelleCase(15);
+		Case c16 = this.nouvelleCase(16);
 		
 		this.addLigne(c1,c2,c3,c4);
 		this.addLigne(c5,c6,c7,c8);
@@ -38,6 +40,12 @@ public class Plateau {
 		this.addLigne(c4,c7,c10,c13);
 	}
 	
+	private Case nouvelleCase(int n){
+		Case c = new Case(n);
+		this.cases.add(c);
+		return c;
+	}
+	
 	private void addLigne(Case c1, Case c2, Case c3, Case c4){
 		Case [] l ={c1,c2,c3,c4}; 
 		try {
@@ -46,6 +54,15 @@ public class Plateau {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public Case choixCase(int n){
+		for(Case c:this.cases){
+			if(c.getId() == n){
+				return c;
+			}
+		}
+		return null;
 	}
 	
 	public ArrayList<Ligne> getLignes(){
