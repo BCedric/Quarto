08f5f52af58167178418331479e3972d8532e866
BCedric <skalimero@gmx.fr>
2015-10-22 09:21:00 +0200

    Création du Main

--- src/Jeu/Main.java
+++ src/Jeu/Main.java
@@ -1,13 +1,41 @@
 package Jeu;
 
+import java.util.Scanner;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Jeu j = new Jeu();
+		Case c;
+		Piece p;
+		int n;
+		while(true){
+			if(j.getActif() == j.getJ1()){
+				System.out.println("J2 choisi une piece");
+				n = sc.nextInt();
+				p = j.choixPiece(n);
+				j.getJ2().choisirPieceAction(p);
+				System.out.println("J1 choisi une case");
+				n = sc.nextInt();
+				c = j.choixCase(n);
+				j.getJ1().choisirCaseAction(c);
+				j.changerActif();
+			} else {
+				System.out.println("J1 choisi une piece");
+				n = sc.nextInt();
+				p = j.choixPiece(n);
+				j.getJ1().choisirPieceAction(p);
+				System.out.println("J2 choisi une case");
+				n = sc.nextInt();
+				c = j.choixCase(n);
+				j.getJ2().choisirCaseAction(c);
+				j.changerActif();
+			}
+		}
 	}
 
 }
