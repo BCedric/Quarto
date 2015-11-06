8a81482553e59d372bd45995b57f69cf76fd5c64
BCedric <skalimero@gmx.fr>
2015-11-06 15:07:23 +0100

    Ajout de la vue Case

---
+++ src/Interface/CaseIHM.java
@@ -0,0 +1,40 @@
+package Interface;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Paint;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import Jeu.Jeu;
+
+public class CaseIHM extends JPanel{
+	
+	private Jeu j;
+	private int id;
+	
+	public CaseIHM(Jeu j, int id){
+		this.id = id;
+		this.j =j;
+		setLayout(new FlowLayout());
+		this.setPreferredSize(new Dimension(85, 85));
+		
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Jeu getJ() {
+		return j;
+	}
+	
+	
+}
