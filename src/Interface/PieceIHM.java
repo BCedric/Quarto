e787bdb0bd4eea714f3db778edf2940b691b1559
BCedric <skalimero@gmx.fr>
2015-11-06 15:07:34 +0100

    Ajout de la vue Piece

---
+++ src/Interface/PieceIHM.java
@@ -0,0 +1,52 @@
+package Interface;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import Jeu.Jeu;
+import Jeu.Piece;
+
+public class PieceIHM extends JPanel{
+	private int id;
+	private Image img;
+	private Jeu j;
+	private JComponent conteneur;
+	
+	public PieceIHM(String img, int i, Jeu j, JPanel conteneur){
+		this.setConteneur(conteneur);
+		this.id = i;
+		this.j = j;
+		this.img = new ImageIcon(img).getImage();
+		this.setPreferredSize(new Dimension(80, 80));
+	}
+	
+	 public Jeu getJ() {
+		return j;
+	}
+
+	public void paintComponent(Graphics g) {
+		    g.drawImage(img, 0, 0, null);
+	}
+	 
+	public int getId() {
+		return id;
+	}
+
+	public JComponent getConteneur() {
+		return conteneur;
+	}
+
+	public void setConteneur(JComponent conteneur) {
+		this.conteneur = conteneur;
+	}
+
+}
