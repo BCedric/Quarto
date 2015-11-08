7c023415b3ca858d635ac08fbbbabff8c6e68de0
BCedric <skalimero@gmx.fr>
2015-11-08 18:46:48 +0100

    Ajout d'une méthode qui renvoie les caractéristiques communes sur une ligne

--- src/Jeu/Ligne.java
+++ src/Jeu/Ligne.java
@@ -27,32 +27,37 @@ public class Ligne {
 	
 	public boolean finPartie(){
 		if(this.estComplète()){
-			ArrayList<String> caracCommunesGlobales = new ArrayList<String>();
-			String s;
-			Piece p1;
-			Piece p2;
-			caracCommunesGlobales.add("couleur");
-			caracCommunesGlobales.add("taille");
-			caracCommunesGlobales.add("interieur");
-			caracCommunesGlobales.add("forme");
-			for(int i=1;i<cases.size();++i){
-				p1 = cases.get(i-1).getPiece();
-				p2 = cases.get(i).getPiece();
-				ArrayList<String> caracCommunes = p1.caracteristiquesCommunes(p2);
-				for(int j = caracCommunesGlobales.size()-1;j>=0 ;--j){
-					s=caracCommunesGlobales.get(j);
-					if(!caracCommunes.contains(s)){
-						caracCommunesGlobales.remove(s);
-					}
-				}
-				if(caracCommunesGlobales.isEmpty()){
-					return false;
-				}
+			if(this.caracteristiquesCommunes().isEmpty()){
+				return false;
 			}
+
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	public ArrayList<String> caracteristiquesCommunes(){
+		ArrayList<String> caracCommunesGlobales = new ArrayList<String>();
+		String s;
+		Piece p1;
+		Piece p2;
+		caracCommunesGlobales.add("couleur");
+		caracCommunesGlobales.add("taille");
+		caracCommunesGlobales.add("interieur");
+		caracCommunesGlobales.add("forme");
+		for(int i=1;i<cases.size();++i){
+			p1 = cases.get(i-1).getPiece();
+			p2 = cases.get(i).getPiece();
+			ArrayList<String> caracCommunes = p1.caracteristiquesCommunes(p2);
+			for(int j = caracCommunesGlobales.size()-1;j>=0 ;--j){
+				s=caracCommunesGlobales.get(j);
+				if(!caracCommunes.contains(s)){
+					caracCommunesGlobales.remove(s);
+				}
+			}
+		}
+		return caracCommunesGlobales;
 	}
 
 }
