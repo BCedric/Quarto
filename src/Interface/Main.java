06ef61abfc437f9a139ac3e2a18f085354b47301
BCedric <skalimero@gmx.fr>
2015-11-12 10:14:01 +0100

    Modification du Main

--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -2,16 +2,32 @@ package Interface;
 
 import javax.swing.JFrame;
 
+import IA.NoeudMax;
+import Jeu.Case;
+import Jeu.CaseOccupeeException;
 import Jeu.Jeu;
+import Jeu.Piece;
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
-		 new Jeu(true).jouer();
+		 Jeu j = new Jeu(true);
+		 j.jouer();
+//		 Piece p = j.choixPiece(1);
+//		 j.getNonActif().choisirPieceAction(p);
+//		 j.getActif().choisirCaseAction(j.choixCase(1));
+//		 Case ca = null;
+//		 for(Case c : j.getPlateau().getCases()){
+//			 if(c.getId() == 1){
+//				 ca =c;
+//				 break;
+//			 }
+//		 }
+//		 new NoeudMax(null).annulerCoup(j, ca);
 	}
 
 }
