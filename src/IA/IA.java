3c462a6c8a19972c86774e99dfe8b1f89a23078f
BCedric <skalimero@gmx.fr>
2015-11-17 11:49:08 +0100

    Création classe IA

---
+++ src/IA/IA.java
@@ -0,0 +1,30 @@
+package IA;
+
+import Jeu.Jeu;
+import Jeu.Joueur;
+
+public class IA {
+
+	private Jeu jeu;
+	
+	public IA(Jeu j){
+		this.jeu =j;
+	}
+	
+	public int evaluation(Jeu j){
+		//TODO 
+		return 0;
+	}
+	
+	public void jouerPiece(){
+		
+	}
+	
+	public void choisirCase(){
+		
+	}
+
+	public Jeu getJeu() {
+		return jeu;
+	}
+}
