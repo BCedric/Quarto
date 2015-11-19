7ef754a56aeb22178ea55ad4cb608a25d257c754
BCedric <skalimero@gmx.fr>
2015-11-19 08:09:21 +0100

    Création de la classe IA

--- src/IA/IA.java
+++ src/IA/IA.java
@@ -1,26 +1,69 @@
 package IA;
 
+import java.util.ArrayList;
+
+import Interface.FenetreFinDePartie;
+import Jeu.Case;
 import Jeu.Jeu;
 import Jeu.Joueur;
+import Jeu.Ligne;
+import Jeu.Piece;
 
-public class IA {
+public abstract class IA {
 
-	private Jeu jeu;
+	protected Joueur j;
+	protected Jeu jeu;
+	protected int profondeur = 5;
 	
-	public IA(Jeu j){
-		this.jeu =j;
+	public IA(Jeu jeu, Joueur j){
+		this.jeu =jeu;
+		this.j = j;
 	}
 	
-	public int evaluation(Jeu j){
-		//TODO 
-		return 0;
-	}
+	public abstract int evaluation(Jeu j);
 	
 	public void jouerPiece(){
-		
+		NoeudCaseMax racine = new NoeudCaseMax(null, null, this);
+		ArrayList<Case> coups = new ArrayList<Case>();
+		for(Case c:this.jeu.getPlateau().getCases()){
+			if(c.estVide()) coups.add(c);
+		}
+		racine.calculIA(this.jeu, coups, this.profondeur);
+		for(NoeudAbstrait n:racine.fils){
+			if(n.getValeur() == racine.getValeur()){
+				Case c = ((NoeudPieceAbstrait) n).getCoup();
+				jeu.getVue().jouerPiece(jeu.getVue().getPlateau().getCase(c.getId()));
+				j.choisirCaseAction(c);
+				if(jeu.isFinPartie()){
+					for(Ligne l:jeu.getPlateau().getLignes()){
+						if(l.finPartie()){
+							new FenetreFinDePartie(jeu,l).setVisible(true);
+							jeu.getVue().finPartie();
+						}
+						
+					}
+					
+				}
+				break;
+				
+			}
+		}
 	}
 	
-	public void choisirCase(){
+	public void choisirPiece(){
+		NoeudPieceMax racine = new NoeudPieceMax(null, null, this);
+		ArrayList<Piece> coups = (ArrayList<Piece>) this.jeu.getPieces().clone();
+		racine.calculIA(this.jeu, coups, this.profondeur);
+		for(NoeudAbstrait n:racine.fils){
+			if(n.getValeur() == racine.getValeur()){
+				System.out.println(racine .getValeur());
+				Piece p = ((NoeudCaseAbstrait) n).getCoup();
+				this.j.choisirPieceAction(p);
+				jeu.getVue().choisirPiece(jeu.getVue().getPiecesDispo().getPiece(p.getId()));
+				break;
+				
+			}
+		}
 		
 	}
 
---
+++ src/IA/IA1.java
@@ -0,0 +1,23 @@
+package IA;
+
+import Jeu.Jeu;
+import Jeu.Joueur;
+
+public class IA1 extends IA{
+
+	public IA1(Jeu jeu, Joueur j) {
+		super(jeu, j);
+	}
+
+	@Override
+	public int evaluation(Jeu j) {
+		int i = (int) (Math.random()*100)+1;
+		if(j.isFinPartie() && j.getNonActif() == this.j){
+			i+=20;
+		} else {
+			i-=20;
+		}
+		return i;
+	}
+
+}
